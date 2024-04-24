6e92adf26a32eddbcff8a39c05d9345975f1f35e
blueheart48 <megert.roger@gmail.com>
2024-04-24 15:51:36 +0200

    Quartz sync: Apr 24, 2024, 3:51 PM

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -48,7 +48,7 @@ const defaultOptions: GraphOptions = {
     repelForce: 10,
     centerForce: 0.5,
     linkDistance: 100,
-    fontSize: 0.4,
+    fontSize: 0.8,
     opacityScale: 0.01,
     showTags: true,
     removeTags: [],
